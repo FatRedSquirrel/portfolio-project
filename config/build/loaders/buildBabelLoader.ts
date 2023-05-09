481da6fddeb011138c12397f606b2ea6d7fd103d
Олег Шевченко <103051547+FatRedSquirrel@users.noreply.github.com>
2023-05-09 12:36:46 +0300

    configure babel loader for typescript & fork ts checker & babelRemovePropsPlugin

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -1,10 +1,15 @@
-import { BuildOptions } from "../types/config";
+import { BuildOptions } from '../types/config';
+import babelRemovePropsPlugin from '../../babel/babelRemovePropsPlugin';
 
-export function buildBabelLoader(options: BuildOptions) {
-  const { isDev } = options;
+interface BuildBabelLoaderOptions extends BuildOptions {
+  isTsx: boolean
+}
+
+export function buildBabelLoader(options: BuildBabelLoaderOptions) {
+  const { isDev, isTsx } = options;
 
   return {
-    test: /\.(js|jsx|tsx)$/,
+    test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
     exclude: /node_modules/,
     use: [
       {
@@ -19,10 +24,21 @@ export function buildBabelLoader(options: BuildOptions) {
                 keyAsDefaultValue: true,
               },
             ],
-            isDev && require.resolve('react-refresh/babel')
+            [
+              '@babel/plugin-transform-typescript',
+              { isTsx },
+            ],
+            '@babel/plugin-transform-runtime',
+            isTsx && [
+              babelRemovePropsPlugin,
+              {
+                props: ['data-testid'],
+              },
+            ],
+            isDev && require.resolve('react-refresh/babel'),
           ].filter(Boolean),
         },
       },
     ],
-  };;
+  };
 }
