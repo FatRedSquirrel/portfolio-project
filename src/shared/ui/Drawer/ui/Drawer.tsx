ae78e908c70d65872b320d5bafdec9d4d1569802
Олег Шевченко <103051547+FatRedSquirrel@users.noreply.github.com>
2023-05-10 10:01:42 +0300

    refactor modal & drawer, create hook useModal

--- src/shared/ui/Drawer/ui/Drawer.tsx
+++ src/shared/ui/Drawer/ui/Drawer.tsx
@@ -1,6 +1,7 @@
 import classNames from 'shared/lib/classNames/classNames';
 import React, { memo, ReactNode } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
+import { useModal } from 'shared/lib/hooks/useModal';
 import { Overlay } from '../../Overlay';
 import cls from './Drawer.module.scss';
 import { Portal } from '../../Portal';
@@ -8,8 +9,9 @@ import { Portal } from '../../Portal';
 interface DrawerProps {
     className?: string;
     children: ReactNode;
-    isOpen?: boolean;
-    onClose?: () => void;
+    isOpen: boolean;
+    onClose: () => void;
+    lazy?: boolean
 }
 
 export const Drawer = memo((props: DrawerProps) => {
@@ -18,13 +20,31 @@ export const Drawer = memo((props: DrawerProps) => {
     children,
     onClose,
     isOpen,
+    lazy,
   } = props;
   const { theme } = useTheme();
 
+  const { close, isClosing, isMounted } = useModal({
+    isOpen,
+    onClose,
+    animationDelay: 200,
+    lazy,
+  });
+
+  if (lazy && !isMounted) return null;
+
   return (
     <Portal>
-      <div className={classNames(cls.Drawer, isOpen && cls.opened, className, theme, 'app_drawer')}>
-        <Overlay onClick={onClose} />
+      <div className={classNames(
+        cls.Drawer,
+        isOpen && cls.opened,
+        isClosing && cls.isClosing,
+        className,
+        theme,
+        'app_drawer',
+      )}
+      >
+        <Overlay onClick={close} />
         <div
           className={cls.content}
         >
--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -1,18 +1,16 @@
 import classNames from 'shared/lib/classNames/classNames';
-import React, {
-  MutableRefObject,
-  ReactNode, useCallback, useEffect, useRef, useState,
-} from 'react';
+import { ReactNode } from 'react';
 import { Portal } from 'shared/ui/Portal';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Overlay } from 'shared/ui/Overlay';
+import { useModal } from 'shared/lib/hooks/useModal';
 import cls from './Modal.module.scss';
 
 interface ModalProps {
     className?: string;
     children?: ReactNode;
-    isOpen?: boolean;
-    onClose?: () => void;
+    isOpen: boolean;
+    onClose: () => void;
     lazy?: boolean;
 }
 
@@ -27,48 +25,14 @@ export const Modal = (props: ModalProps) => {
     lazy,
   } = props;
 
-  const [isClosing, setIsClosing] = useState(false);
-  const [isMounted, setIsMounted] = useState(false);
-  const timerRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>;
   const { theme } = useTheme();
 
-  useEffect(() => {
-    if (isOpen) {
-      setIsMounted(true);
-    }
-  }, [isOpen]);
-
-  const closeHandler = useCallback(() => {
-    if (onClose) {
-      setIsClosing(true);
-      timerRef.current = setTimeout(() => {
-        onClose();
-        setIsClosing(false);
-      }, ANIMATION_DELAY);
-    }
-  }, [onClose]);
-
-  // Новые ссылки!!!
-  const onKeyDown = useCallback((e: KeyboardEvent) => {
-    if (e.key === 'Escape') {
-      closeHandler();
-    }
-  }, [closeHandler]);
-
-  const onContentClick = (e: React.MouseEvent) => {
-    e.stopPropagation();
-  };
-
-  useEffect(() => {
-    if (isOpen) {
-      window.addEventListener('keydown', onKeyDown);
-    }
-
-    return () => {
-      clearTimeout(timerRef.current);
-      window.removeEventListener('keydown', onKeyDown);
-    };
-  }, [isOpen, onKeyDown]);
+  const { close, isClosing, isMounted } = useModal({
+    animationDelay: ANIMATION_DELAY,
+    onClose,
+    lazy,
+    isOpen,
+  });
 
   if (lazy && !isMounted) {
     return null;
@@ -87,11 +51,8 @@ export const Modal = (props: ModalProps) => {
         )
       }
       >
-        <Overlay onClick={onClose} />
-        <div
-          className={cls.content}
-          onClick={onContentClick}
-        >
+        <Overlay onClick={close} />
+        <div className={cls.content}>
           {children}
         </div>
       </div>
