55528f612e2fdb166e22966b4ba2accfe6cb5012
Олег Шевченко <103051547+FatRedSquirrel@users.noreply.github.com>
2023-05-25 10:56:07 +0300

    build selector types - new can use with arguments

--- cypress/support/commands/profile.ts
+++ cypress/support/commands/profile.ts
@@ -1,12 +1,12 @@
 import { USER_LOCALSTORAGE_KEY } from '../../../src/shared/const/localstorage';
 
 export const updateProfile = () => {
-  cy.selectByTestId('')
-}
+  cy.selectByTestId('');
+};
 
 export const resetProfile = (id: string) => cy.request({
   method: 'POST',
-  url: 'http://localhost:8000/login'
+  url: 'http://localhost:8000/login',
 }).then(({ body }) => {
   window.localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(body));
 
