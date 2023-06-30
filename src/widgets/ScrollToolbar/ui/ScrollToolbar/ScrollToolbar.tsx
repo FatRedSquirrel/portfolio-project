ff5297e02863177901fb0f9e6f6b521496f9888e
Олег Шевченко <103051547+FatRedSquirrel@users.noreply.github.com>
2023-06-30 09:46:32 +0300

    hide scroll top button

--- src/widgets/ScrollToolbar/ui/ScrollToolbar/ScrollToolbar.tsx
+++ src/widgets/ScrollToolbar/ui/ScrollToolbar/ScrollToolbar.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { classNamesRedesigned } from '@/shared/lib/classNames/classNames';
 import cls from './ScrollToolbar.module.scss';
 import { VStack } from '@/shared/ui/redesigned/Stack';
@@ -11,6 +11,31 @@ interface ScrollToolbarProps {
 export const ScrollToolbar = memo((props: ScrollToolbarProps) => {
   const { className } = props;
 
+  const [goUpShown, setGoUpShown] = useState(false);
+
+  const observePageScroll = () => {
+    let shown = false;
+
+    return () => {
+      const offsetTop = window.scrollY;
+      if (offsetTop > 1200 && !shown) {
+        shown = true;
+        setGoUpShown(true);
+      } else if (offsetTop < 1200 && shown) {
+        shown = false;
+        setGoUpShown(false);
+      }
+    };
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', observePageScroll());
+
+    return () => {
+      document.removeEventListener('scroll', observePageScroll());
+    };
+  }, []);
+
   return (
     <VStack
       justify="center"
@@ -18,7 +43,7 @@ export const ScrollToolbar = memo((props: ScrollToolbarProps) => {
       max
       className={classNamesRedesigned(cls.ScrollToolbar, {}, [className])}
     >
-      <ScrollToTopButton />
+      {goUpShown && <ScrollToTopButton />}
     </VStack>
   );
 });
