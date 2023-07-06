0f2b52ac97d5de393034a6e4b829f8f4140901ea
Олег Шевченко <103051547+FatRedSquirrel@users.noreply.github.com>
2023-07-06 11:31:27 +0300

    add simple rating (redesigned)

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,12 +1,14 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import { Rating } from 'react-simple-star-rating';
 import { ArticleDetails } from '@/entities/Article';
 import { DynamicModuleLoader, ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import {
   articleCommentsReducer,
 } from '@/pages/ArticleDetailsPage/model/slice/articleDetailsCommentsSlice';
 import { Page } from '@/widgets/Page';
+import { Text } from '@/shared/ui/redesigned/Text';
 import {
   ArticleDetailsPageHeader,
 } from '@/pages/ArticleDetailsPage/ui/ArticleDetailsPageHeader/ArticleDetailsPageHeader';
@@ -22,6 +24,8 @@ import { StickyContentLayout } from '@/shared/layouts/StickyContentLayout';
 import { DetailsContainer } from '../DetailsContainer';
 import { AdditionalInfoContainer } from '../AdditionalInfoContainer/ui/AdditionalInfoContainer';
 import { Button } from '@/shared/ui/redesigned/Button';
+import { VStack } from '@/shared/ui/redesigned/Stack';
+import { Card } from '@/shared/ui/redesigned/Card';
 
 const reducers: ReducersList = {
   articleComments: articleCommentsReducer,
@@ -35,6 +39,8 @@ const ArticleDetailsPage = () => {
 
   const { t } = useTranslation();
 
+  const [rating, setRating] = useState(0);
+
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
       <ToggleFeatures
@@ -44,7 +50,17 @@ const ArticleDetailsPage = () => {
             content={(
               <Page noInitialScroll>
                 <DetailsContainer />
-                <ArticleRating />
+                <br />
+                <Card padding='16'>
+                  <VStack align='center' gap="16">
+                    <Text title={rating ? t('Спасибо за оценку!') as string : t('Оцените статью') as string} />
+                    <Rating
+                      initialValue={rating}
+                      onClick={(rate) => setRating(rate)}
+                      transition
+                    />
+                  </VStack>
+                </Card>
                 <ArticleRecommendationsList />
                 <ArticleComments />
               </Page>
@@ -66,17 +82,7 @@ const ArticleDetailsPage = () => {
           <Page>
             <ArticleDetailsPageHeader />
             <ArticleDetails />
-            <ToggleFeatures
-              feature='isArticleRatingEnabled'
-              on={<ArticleRating />}
-              off={(
-                <div className={cls.noRating}>
-                  {t('Оценка статей скоро появится')}
-                  !
-                  (feature flags)
-                </div>
-              )}
-            />
+            <ArticleRating />
             <ArticleRecommendationsList />
             <ArticleComments />
           </Page>
