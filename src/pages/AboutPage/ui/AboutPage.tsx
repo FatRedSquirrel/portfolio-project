dce0014a252c907d185a0970629fc4d2fb5941c6
Олег Шевченко <103051547+FatRedSquirrel@users.noreply.github.com>
2023-07-08 11:59:59 +0300

    add documentation and storybook links

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,12 +1,33 @@
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
+import { Text } from '@/shared/ui/redesigned/Text';
+import { AppLink } from '@/shared/ui/redesigned/AppLink';
 
 const AboutPage = () => {
   const { t } = useTranslation();
 
   return (
     <Page dataTestid='AboutPage'>
-      {t('О сайте')}
+      <Text size='xl' title={t('О сайте') as string} />
+      <div style={{
+        display: 'flex',
+        gap: 20,
+        marginTop: 30,
+      }}
+      >
+        <AppLink
+          target='_blank'
+          to='https://fatredsquirrel.github.io/portfolio-project-wiki/'
+        >
+          {t('Документация')}
+        </AppLink>
+        <AppLink
+          target='_blank'
+          to='https://fatredsquirrel.github.io/portfolio-project/'
+        >
+          Storybook
+        </AppLink>
+      </div>
     </Page>
   );
 };
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,18 +1,33 @@
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
+import { Text } from '@/shared/ui/redesigned/Text';
+import { AppLink } from '@/shared/ui/redesigned/AppLink';
 
 const MainPage = () => {
   const { t } = useTranslation();
-  const [value, setValue] = useState('');
-
-  const onChange = (val: string) => {
-    setValue(val);
-  };
 
   return (
     <Page dataTestid='MainPage'>
-      {t('Главная страница')}
+      <Text size='xl' title={t('Главная страница') as string} />
+      <div style={{
+        display: 'flex',
+        gap: 20,
+        marginTop: 30,
+      }}
+      >
+        <AppLink
+          target='_blank'
+          to='https://fatredsquirrel.github.io/portfolio-project-wiki/'
+        >
+          {t('Документация')}
+        </AppLink>
+        <AppLink
+          target='_blank'
+          to='https://fatredsquirrel.github.io/portfolio-project/'
+        >
+          Storybook
+        </AppLink>
+      </div>
     </Page>
   );
 };
